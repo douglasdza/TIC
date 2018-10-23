8803dce887eb18861568ec1d2bd7096f909bce08
Aluno <dubghlas@outlook.com>
2018-10-23 21:14:41 -0200

    Django atualizado

--- 3º Trimestre/django/Adocao/Adocao/settings.py
+++ 3º Trimestre/django/Adocao/Adocao/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#Cria um endereço absoluto para a pasta static
+# Cria um endereço absoluto para a pasta static
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
